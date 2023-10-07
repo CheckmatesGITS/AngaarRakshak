e4d6b3b7458d29e59f4a6bf6c9c94b0c042d0a50
CheckmatesGITS <146119692+CheckmatesGITS@users.noreply.github.com>
2023-10-07 10:25:28 +0530

    Enchancing Responsive View

--- js/sideMenu.js
+++ js/sideMenu.js
@@ -1,35 +1,32 @@
 let x = window.matchMedia("(max-width: 768px)")
 
 let menuBtn = document.querySelector(`#menuBtn`);
-let menu = document.querySelector(`#dashboard .sideMenu`);
+let closeBtn = document.querySelector(`#closeBtn`);
+
+let menu = document.querySelector(`.sideMenu`);
 let menuList = document.querySelectorAll(`.sideMenu .menu li`);
 
 
+
 function myFunction(x) {
-    if (x.matches) { // If media query matches
-
-        menuBtn.innerHTML = `<i class="ri-menu-2-fill"></i>`;
-        if (menu.offsetWidth != '200px') {
-            menuBtn.addEventListener(`click`, ()=>{
-                menu.style.cssText = "width: 200px;position: fixed;top: 0;left: 0;bottom: 0;z-index: 2;";
-                menuList.style.padding = "5%";
-                menuBtn.style.justifyContent = 'flex-end';
-                menuBtn.innerHTML = `<i class="ri-close-fill"></i>`;
-            });
-        }
-        else {
-            menuBtn.addEventListener(`click`, ()=>{
-                menu.style.cssText = "width: 70px;position: sticky;top: 0;left: 0;bottom: 0;z-index: 2;";
-                menuBtn.style.padding = "25%";
-                menuBtn.innerHTML = `<i class="ri-menu-2-fill"></i>`;
-            });    
-        }
-
-        
-    } 
-    else {
-    }
+  if (x.matches) { // If media query matches
+    closeBtn.innerHTML = `<i class="ri-close-line"></i>`;
+    menuBtn.innerHTML = `<i class="ri-menu-2-fill"></i>`;
+    menuList.forEach(element => {
+      element.style.padding = `5%`;
+    });
+    menuBtn.addEventListener(`click`, () => {
+      menu.style.cssText = `transform:translateX(0);`;
+    })
+    
+    closeBtn.addEventListener(`click`, () => {
+      menu.style.cssText = `transform:translateX(-250px);`;
+    })
   }
-  
+  else {
+    menuBtn.innerHTML = ``;
+  }
+}
+
 myFunction(x) // Call listener function at run time
-x.addEventListener(`change`,myFunction) // Attach listener function on state changes
+x.addEventListener(`change`, myFunction) // Attach listener function on state changes
